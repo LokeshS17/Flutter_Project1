6e4493982dc154746849c6ba367b0fafc46f9664
unknown <lokesh17d@gmail.com>
2025-01-04 21:08:01 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:location/package/choose_location.dart';
+import 'package:location/package/home.dart';
+import 'package:location/package/loading.dart';
+
+void main() {
+  runApp(MaterialApp(
+     /* home: Home(),*/
+    initialRoute: '/home',
+    routes: {
+        '/':(context) => Loading(),
+      '/home':(context)=> Home(),
+      '/location':(context)=>ChooseLocation(),
+    },
+  ));
+}
